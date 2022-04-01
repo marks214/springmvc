7ae724f4ceb9c652d256c2dca5428639af0cfeec
marks214 <aimee.oz.x@gmail.com>
2022-03-31 20:20:25 -0700

    throw RuntimeException when user already exists in db

--- src/main/java/com/dev/app/ws/mobileapp/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/dev/app/ws/mobileapp/ws/service/impl/UserServiceImpl.java
@@ -17,6 +17,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto createUser(UserDto user) {
 
+        if(userRepository.findUserByEmail((user.getEmail())) != null) throw new RuntimeException("Record already exists");
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
